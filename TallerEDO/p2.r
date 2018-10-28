c2d4e2cb79dbd9f09c12ea81e1019797d6b26189
Leyder Vera <41376427+veraele@users.noreply.github.com>
2018-10-28 18:35:24 -0500

    Update p2.r

--- TallerEDO/p2.r
+++ TallerEDO/p2.r
@@ -16,9 +16,9 @@ print(data.frame(X = x, Y = polyval(pT,x)))
 
 x <- seq(-2, 2, length.out=100)
 pTa <- polyval(pT, x)
-plot(x, pTa, type = "l", col = "gray", lwd = 3)
+plot(x, pTa, type = "l", col = "gray",main="Comparación",xlab="X",ylab="Y", lwd = 3)
 lines(x, exact(x), col = "orange" ,lwd = 3)
-
+legend(-2,6,c('Taylor','Exacta'),lty=c(1,1),lwd=c(2.5,2.5),col=c('gray','orange'))
 cat("\n")
 print("con h= 0.1:")
 cat("valor real       valor con taylor     error\n")
